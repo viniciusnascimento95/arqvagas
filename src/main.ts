03598e009f8e1fadec472f4ec30e4acc2a086d67
Vinicius Fernandes <nascimento.vinicius32@gmail.com>
2024-11-19 20:59:49 -0300

    init project

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('arqvagas API')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('routes')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3333);
+}
+bootstrap();
