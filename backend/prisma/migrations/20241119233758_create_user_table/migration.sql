6a4b9d9ae5aeead37afa446e29fe3b757cbe6772
Vinicius Fernandes <nascimento.vinicius32@gmail.com>
2025-01-06 23:27:13 -0300

    add: add columns in table User

--- backend/prisma/migrations/20241119233758_create_user_table/migration.sql
+++ backend/prisma/migrations/20241119233758_create_user_table/migration.sql
@@ -2,10 +2,15 @@
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
-    "name" TEXT,
+    "name" TEXT NOT NULL,
+    "school" TEXT,
+    "init_date_school" TIMESTAMP(3),
+    "end_date_school" TIMESTAMP(3),
+    "software_skills" TEXT[],
+    "personal_skills" TEXT[],    
+    "portfolio_url" TEXT, 
     "password" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
