157c2560c801896b661db854e6044792a4b93cb2
Vinicius Fernandes <nascimento.vinicius32@gmail.com>
2025-01-06 22:34:28 -0300

    add: ajustes pagina de profile

--- mobile/app/(tabs)/profile.tsx
+++ mobile/app/(tabs)/profile.tsx
@@ -1,12 +1,13 @@
 import { Box } from '@/components/ui/box';
+import { Divider } from '@/components/ui/divider';
 import { HStack } from '@/components/ui/hstack';
 import { Text } from '@/components/ui/text';
 import { VStack } from '@/components/ui/vstack';
 
 export default function TabThreeScreen() {
   return (
-    <Box className="flex-1 bg-secondary-100 md:bg-secondary-0 md:items-center md:justify-center m-6">
-      <HStack>
+    <Box className="flex-1 bg-secondary-100 md:bg-secondary-0 md:items-center md:justify-center my-10">
+      <HStack className="md:bg-secondary-0 md:items-center md:justify-center m-4">
         <VStack>
           <HStack space='sm' className='justify-between'>
             <Text style={{ fontSize: 18, fontWeight: "bold" }}>Nome</Text>
@@ -21,6 +22,33 @@ export default function TabThreeScreen() {
           </Text>
         </VStack>
       </HStack>
+      <Divider className="my-0.5" />
+      <VStack className="md:bg-secondary-0 md:items-center md:justify-center m-4">
+        <HStack space='sm' className='justify-between'>
+          <Text style={{ fontSize: 18, fontWeight: "bold" }}>Escolaridade</Text>
+          <Text style={{ fontSize: 12, fontWeight: "light" }}>Superior em Arquitetura</Text>
+        </HStack>
+        <HStack space='sm' className='justify-between'>
+          <Text style={{ fontSize: 18, fontWeight: "bold" }}>Experiência Profissional</Text>
+          <Text style={{ fontSize: 12, fontWeight: "light" }}>2 anos como Arquiteto Júnior</Text>
+        </HStack>
+        <HStack space='sm' className='justify-between'>
+          <Text style={{ fontSize: 18, fontWeight: "bold" }}>Habilidades</Text>
+          <Text style={{ fontSize: 12, fontWeight: "light" }}>AutoCAD, SketchUp, Revit</Text>
+        </HStack>
+        <HStack space='sm' className='justify-between'>
+          <Text style={{ fontSize: 18, fontWeight: "bold" }}>Certificações</Text>
+          <Text style={{ fontSize: 12, fontWeight: "light" }}>Certificação em Design Sustentável</Text>
+        </HStack>
+        <HStack space='sm' className='justify-between'>
+          <Text style={{ fontSize: 18, fontWeight: "bold" }}>Portfólio</Text>
+          <Text style={{ fontSize: 12, fontWeight: "light" }}>www.portfolio-arquitetura.com</Text>
+        </HStack>
+        <HStack space='sm' className='justify-between'>
+          <Text style={{ fontSize: 18, fontWeight: "bold" }}>Disponibilidade</Text>
+          <Text style={{ fontSize: 12, fontWeight: "light" }}>Imediata</Text>
+        </HStack>
+      </VStack>
     </Box>
   );
 }
