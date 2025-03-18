c4e1a7e81b99507ec7f2be3788859c9854fba68e
Vinicius Fernandes <nascimento.vinicius32@gmail.com>
2025-03-18 00:15:35 -0300

    bug

--- webvagas/src/services/api.ts
+++ webvagas/src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: `${process.env.BASE_URL_API}:${process.env.NEXT_PUBLIC_PORT_API}`,
+  baseURL: 'http://api.viniciusfernandes.site:3333',
 })
